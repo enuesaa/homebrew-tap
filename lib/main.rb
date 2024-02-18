4bc55fa45204ab5c429e7a27a8c30bdd031083e8
nua <enuesaa@gmail.com>
2024-02-18 17:53:59 +0900

    chore: add target

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 require_relative 'program'
 
-for name in ['pinit', 'cpbuf']
+for name in ['pinit', 'cpbuf', 'difii']
   program = Program.new('enuesaa', name)
   checksums = program.fetch_checksums
   version = program.fetch_version
