6476511670cffa85e638ab65fefb47dbd4f31b5e
nua <enuesaa@gmail.com>
2024-02-24 19:45:48 +0900

    feat: look up tryserve

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # homebrew-tap
 homebrew-tap repository for my personal cli tools.
-
-## メモ
-- https://zenn.dev/teitei_tk/articles/9787347abceb02
--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 require_relative 'program'
 
-for name in ['pinit', 'cpbuf', 'difii', 'imgpack', 'walkin']
+for name in ['pinit', 'cpbuf', 'difii', 'imgpack', 'walkin', 'tryserve']
   program = Program.new('enuesaa', name)
   checksums = program.fetch_checksums
   version = program.fetch_version
