f18c102975a97dcf5e477c4639133ddff5ec194c
nua <enuesaa@gmail.com>
2024-02-25 23:35:14 +0900

    chore: remove tryserve because i may rename it

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 require_relative 'program'
 
-for name in ['pinit', 'cpbuf', 'difii', 'imgpack', 'walkin', 'tryserve']
+for name in ['pinit', 'cpbuf', 'difii', 'imgpack', 'walkin']
   program = Program.new('enuesaa', name)
   checksums = program.fetch_checksums
   version = program.fetch_version
