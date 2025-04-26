9678636afe1afa883290f34c32c0963ad391e437
nua <enuesaa@gmail.com>
2025-04-26 23:34:01 +0900

    chore: add cywagon

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 require_relative 'program'
 
-for name in ['pinit', 'cpbuf', 'difii', 'imgpack', 'walkhttp', 'loadii', 'dotslide']
+for name in ['pinit', 'cpbuf', 'difii', 'imgpack', 'walkhttp', 'loadii', 'dotslide', 'cywagon']
   program = Program.new('enuesaa', name)
   checksums = program.fetch_checksums
   version = program.fetch_version
