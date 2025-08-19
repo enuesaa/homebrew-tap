31c581f0767d9cd3121c312c98c22fb8e0f563d1
nua <enuesaa@gmail.com>
2025-08-20 00:39:32 +0900

    chore: imgpack をの release は整備中であり自動化できないので手動更新する

--- Formula/imgpack.rb
+++ Formula/imgpack.rb
@@ -1,21 +1,12 @@
 class Imgpack < Formula
   desc ""
   homepage ""
-  version "v0.0.6"
+  version "v0.1.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/enuesaa/imgpack/releases/download/v0.0.6/imgpack_0.0.6_darwin_amd64.tar.gz"
-      sha256 "71f9064cea1a341d813755bc0009c44563fc9f88acce95dc327f17e080cc337d"
-
-      def install
-        bin.install "imgpack"
-      end
-    end
-
     if Hardware::CPU.arm?
-      url "https://github.com/enuesaa/imgpack/releases/download/v0.0.6/imgpack_0.0.6_darwin_arm64.tar.gz"
-      sha256 "fc23fff4518952cedd3c44f147b7761d1b969a10650f9cf34a92515fa17e18fb"
+      url "https://github.com/enuesaa/imgpack/releases/download/v0.1.0/imgpack-v0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "42a8863e93f76794faf651dfc724ebbe40585fe8b895f5c5f95ae2d1039f07fd"
 
       def install
         bin.install "imgpack"
--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 require_relative 'program'
 
-for name in ['pinit', 'cpbuf', 'difii', 'imgpack', 'walkhttp', 'loadii', 'dotslide', 'cywagon']
+for name in ['pinit', 'cpbuf', 'difii', 'walkhttp', 'loadii', 'dotslide', 'cywagon']
   puts "#{name}: start"
   program = Program.new('enuesaa', name)
   checksums = program.fetch_checksums
